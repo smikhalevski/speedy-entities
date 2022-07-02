a18fe402760b67de1f7791e4bf94fec945e288eb
smikhalevski <smikhalevski@gmail.com>
2022-07-03 00:41:43 +0300

    Better pack/unpack data

--- pack-data.js
+++ pack-data.js
@@ -5,12 +5,21 @@ const legacyHtmlEntities = require('./src/main/data/legacy-html-entities.json');
 
 const genDir = path.join(__dirname, './src/main/gen');
 
-fs.mkdirSync(genDir, {recursive: true});
+const modernHtmlEntities = Object.entries(htmlEntities).filter(([key]) => !legacyHtmlEntities[key]);
 
-fs.writeFileSync(path.join(genDir, 'html-entities.json'), packEntries(Object.entries(htmlEntities).filter(([key]) => !(key in legacyHtmlEntities))));
+fs.mkdirSync(genDir, { recursive: true });
+
+fs.writeFileSync(path.join(genDir, 'html-entities.json'), packEntries(modernHtmlEntities));
 
 fs.writeFileSync(path.join(genDir, 'legacy-html-entities.json'), packEntries(Object.entries(legacyHtmlEntities)));
 
 function packEntries(entries) {
-  return JSON.stringify(entries.map(([key, value]) => key + ' ' + (value.length === 1 ? value.charCodeAt(0) : value.charCodeAt(0) * 0xffff + value.charCodeAt(1)).toString(36)).join(' '));
+  return JSON.stringify(
+    entries
+      .map(([key, value]) => {
+        const raw = value.length === 1 ? value.charCodeAt(0) : value.charCodeAt(0) * 0xffff + value.charCodeAt(1);
+        return key + ' ' + raw.toString(36);
+      })
+      .join(' ')
+  );
 }
--- src/main/unpackData.ts
+++ src/main/unpackData.ts
@@ -1,13 +1,14 @@
 /**
  * Unpacks a mapping packed at build time.
  */
-export function unpackData(data: string): Record<string, string> {
-  const unpackedData: Record<string, string> = {};
+export function unpackData(data: string): { [name: string]: string } {
+  const unpackedData: { [name: string]: string } = {};
   const tokens = data.split(' ');
+  const fromCharCode = String.fromCharCode;
 
   for (let i = 0; i < tokens.length; i += 2) {
     const raw = parseInt(tokens[i + 1], 36);
-    unpackedData[tokens[i]] = raw > 0xffff ? String.fromCharCode(raw / 0xffff, raw % 0xffff) : String.fromCharCode(raw);
+    unpackedData[tokens[i]] = raw > 0xffff ? fromCharCode(raw / 0xffff, raw % 0xffff) : fromCharCode(raw);
   }
   return unpackedData;
 }
